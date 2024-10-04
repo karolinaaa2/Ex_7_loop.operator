eec85916e2592d9c89e1602525d6e260607b5476
karolinaaa2 <hryhorchuk.karolina@chnu.edu.ua>
2024-10-05 00:56:21 +0300

    Add project files.

---
+++ Ex_7_loop.operator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть максимальне число для послідовності Фібоначчі: ");
+        int maxNumber = int.Parse(Console.ReadLine());
+
+        int a = 0, b = 1, sum = 0, count = 0;
+
+        while (a < maxNumber)
+        {
+            sum += a;
+            count++;
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+
+        Console.WriteLine($"Сума чисел Фібоначчі, менших за {maxNumber}: {sum}");
+        Console.WriteLine($"Кількість елементів: {count}");
+    }
+}
+
